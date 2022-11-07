c842fc52b1755a3e9d059fe8a3c314338458da56
Iwwww <mike.yolgin@gmail.com>
2022-11-07 22:50:14 +0300

    Update

--- src/Menu/Menu.hpp
+++ src/Menu/Menu.hpp
@@ -1,51 +1,48 @@
 #pragma once
+#include <any>
 #include <iostream>
 #include <string>
-#include <any>
 #include <vector>
 
 namespace YMM {
 
-    class Menu {
-        public:
-            using func = void(*)(std::vector<std::any>);
-            Menu(std::string lable, func function);
-            Menu(std::string lable, std::vector<Menu> menus);
-            Menu(std::string lable, func function, std::vector<std::any> params);
-            Menu(std::string lable, std::vector<Menu> menus, std::vector<std::any> params);
-            Menu(const Menu& menu);
+class Menu {
+public:
+  using func = void (*)(std::vector<std::any>);
+  Menu(std::string lable, func function);
+  Menu(std::string lable, std::vector<Menu> menus);
+  Menu(std::string lable, func function, std::vector<std::any> params);
+  Menu(std::string lable, std::vector<Menu> menus,
+       std::vector<std::any> params);
+  Menu(const Menu &menu);
 
-            void run(std::vector<std::any> params);
-            void printMenu();
-            std::string getLable();
+  void run(std::vector<std::any> params);
+  void printMenu();
+  std::string getLable();
 
+  template <typename T> static T input() { return input<T>(""); }
 
-            template<typename T>
-            static T input() {
-                return input<T>("");
-            }
+  template <typename T> static T input(std::string str) {
+    T input_str{};
+    bool flag = true;
+    while (flag) {
+      std::cout << str;
+      if (std::cin >> input_str) {
+        flag = false;
+      } else {
+        std::cin.clear();
+        while (std::cin.get() != '\n')
+          ;
+      }
+    }
 
-            template<typename T>
-            static T input(std::string str) {
-                T input_str{};
-                bool flag = true;
-                while (flag) {
-                    std::cout << str;
-                    if (std::cin >> input_str) {
-                        flag = false;
-                    } else {
-                        std::cin.clear();
-                        while (std::cin.get() != '\n');
-                    }
-                }
+    return input_str;
+  }
 
-                return input_str;
-            }
-
-        private:
-            std::vector<std::any> params{};
-            std::string lable{};
-            std::vector<Menu> menus{};
-            func function = nullptr;
-    };
-}
+private:
+  std::vector<std::any> params{};
+  std::string lable{};
+  std::vector<Menu> menus{};
+  func function = nullptr;
+};
+} // namespace YMM
